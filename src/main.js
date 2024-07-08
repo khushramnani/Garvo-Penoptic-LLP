e01944358f580a2efdc601ca0a67f0f64cbaab67
khush <khushramnani@gmail.com>
2024-07-08 09:02:28 +0530

    new section and images added

--- src/main.js
+++ src/main.js
@@ -14,3 +14,35 @@ close.addEventListener('click',()=>{
     coverPage.classList.remove('flex')
     console.log("unclicked");
 })
+
+
+
+// our services 
+
+document.querySelectorAll(".row").forEach(function (elem) {
+    var rotate = 0;
+    var diffrot = 0;
+  
+    elem.addEventListener("mouseleave", function () {
+      gsap.to(elem.querySelector("img"), {
+        opacity: 0,
+        ease: Power3,
+        duration: 0.5,
+      });
+    });
+  
+    elem.addEventListener("mousemove", function (dets) {
+      var diff = dets.clientY - elem.getBoundingClientRect().top;
+      diffrot = dets.clientX - rotate;
+      rotate =  dets.clientX;
+      
+      gsap.to(elem.querySelector("img"), {
+        opacity: 1,
+        ease: Power3,
+        top: diff,
+        left: dets.clientX,
+        rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+        
+    });
+    });
+  });
